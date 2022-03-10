84bf5d8672395f6bee036349b7dea959c98661da
Yehuda-Zvi <jud.zvi85@gmail.com>
2022-03-10 23:17:49 +0200

    final commit

--- src/Success.js
+++ src/Success.js
@@ -2,14 +2,15 @@ import { Flex, Heading } from "@chakra-ui/react"
 import { Navigate } from "react-router-dom"
 
 export default function Success({ user }) {
-  console.log("user: ", user)
   if (!user) {
     return <Navigate to="/" />
   }
 
   return (
-    <Flex height="100vh" justifyContent="center" alignItems="center" direction="column">
-      <Heading color="green.400">{user} You've made it!</Heading>
+    <Flex height="100vh" alignItems="center">
+      <Heading color="green.400" textAlign="center">
+        {user} You've made it!
+      </Heading>
     </Flex>
   )
 }
